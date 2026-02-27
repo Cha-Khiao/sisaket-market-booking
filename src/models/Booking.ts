50458a07e7208e929591d1b6ce47451fb6814c40
Cha-Khiao <p.prasopphol203@gmail.com>
2026-02-27 14:20:44 +0700

    add days

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -1,13 +1,17 @@
 import mongoose, { Schema, models } from 'mongoose';
 
 const bookingSchema = new Schema({
-  bookingId: { type: String, required: true, unique: true },
+  bookingId: { type: String, required: true },
   stallId: { type: String, required: true },
   userId: { type: String, required: true },
   price: { type: Number, required: true },
-  slipImage: { type: String }, // เก็บรูปสลิปเป็น Base64 String
-  ocrPassed: { type: Boolean, default: false }, // OCR ตรวจผ่านไหม
+  
+  // ⚡ เพิ่มบรรทัดนี้ เพื่อให้ฐานข้อมูลยอมรับข้อมูลวันเสาร์-อาทิตย์
+  bookingDays: { type: [String], default: [] }, 
+  
   status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+  slipImage: { type: String, default: '' },
+  ocrPassed: { type: Boolean, default: false }
 }, { timestamps: true });
 
 export default models.Booking || mongoose.model('Booking', bookingSchema);
